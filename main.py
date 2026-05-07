5649d5850c03b6c03b8830c9b8fd385f78374da3
denzelrazafindratsara-crypto <denzelrazafindratsara@gmail.com>
2026-05-08 00:52:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,85 @@
-from fastapi import FastAPI
-import tensorflow as tf
 import numpy as np
-from pydantic import BaseModel
+from tensorflow.keras.models import load_model
+from datetime import timedelta
 
-app = FastAPI()
+model = load_model("crash_model.h5")
 
-# On charge ton modèle .h5
-model = tf.keras.models.load_model("crash_model.h5")
+def predict_logic(data):
 
-# On définit ce que Lovable doit envoyer (ex: les valeurs du capteur)
-class CrashData(BaseModel):
-    inputs: list # Une liste de chiffres
+    sequence_length = 20
 
-@app.get("/")
-def home():
-    return {"message": "L'API de Crash est prête !"}
+    if len(data) < sequence_length:
+        return {
+            "error": "Pas assez de données"
+        }
 
-@app.post("/predict")
-def predict(data: CrashData):
-    # Transformer la liste en tableau pour l'IA
-    input_array = np.array([data.inputs])
-    
-    # Faire la prédiction
-    prediction = model.predict(input_array)
-    
-    return {"resultat": float(prediction[0][0])}
+    # =========================
+    # Préparation IA
+    # =========================
+
+    last_sequence = data[-sequence_length:]
+
+    X = np.array(last_sequence)
+    X = X.reshape(1, sequence_length, 1)
+
+    # =========================
+    # Prediction IA
+    # =========================
+
+    prediction = model.predict(X)
+
+    predicted_multiplier = float(prediction[0][0])
+
+    # =========================
+    # ETA TEMPS
+    # =========================
+
+    # Exemple :
+    # 1 round = 8 secondes
+
+    average_round_time = 8
+
+    threshold = 10.0
+
+    indices = [
+        i for i, val in enumerate(data)
+        if val >= threshold
+    ]
+
+    if len(indices) >= 2:
+
+        gaps = [
+            indices[i] - indices[i - 1]
+            for i in range(1, len(indices))
+        ]
+
+        avg_gap = sum(gaps) / len(gaps)
+
+        last_big_index = indices[-1]
+
+        games_since_last = len(data) - 1 - last_big_index
+
+        eta_rounds = max(
+            0,
+            round(avg_gap - games_since_last)
+        )
+
+        # Conversion secondes
+        eta_seconds = eta_rounds * average_round_time
+
+        # Format HH:MM:SS
+        eta_time = str(
+            timedelta(seconds=eta_seconds)
+        )
+
+    else:
+        eta_time = "00:00:00"
+
+    # =========================
+    # Réponse API
+    # =========================
+
+    return {
+        "prediction": round(predicted_multiplier, 2),
+        "eta": eta_time
+    }
